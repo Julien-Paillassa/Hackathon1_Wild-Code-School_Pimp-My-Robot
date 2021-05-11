b5a45234ac1c34cffed55ecf6e30f35ca6baf4cf
Killian <killian.couraillon@hotmail.fr>
2021-05-11 16:36:57 +0200

    add value in inputs

--- src/Controller/WorkshopController.php
+++ src/Controller/WorkshopController.php
@@ -6,6 +6,8 @@ class WorkshopController extends AbstractController
 {
     public function index()
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        }
         return $this->twig->render('Workshop/index.html.twig');
     }
 }
