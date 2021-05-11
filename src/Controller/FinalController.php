789c33a2b7694106be934ca39791d8ac9884c82a
Matthieu <matthieudejean@hotmail.com>
2021-05-11 17:07:15 +0200

    page final

---
+++ src/Controller/FinalController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class FinalController extends AbstractController
+{
+    public function index()
+    {
+        return $this->twig->render('Success/final.html.twig');
+    }
+}
